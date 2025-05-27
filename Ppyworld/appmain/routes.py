3e8ef1623d8c9f2a24b9a9a7a04bb035ca79aa75
wooj4382 <dlsdn8967@gmail.com>
2025-05-28 03:44:30 +0900

    Add files via upload

--- Ppyworld/appmain/routes.py
+++ Ppyworld/appmain/routes.py
@@ -1,13 +1,8 @@
-from flask import Blueprint, send_from_directory
-
-from appmain import app
-
-from appmain.user.routes import user
-app.register_blueprint(user)
+from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 @main.route('/home')
 def home():
-    return send_from_directory(app.root_path, 'templates/index.html')
+    return render_template('index.html')
